9a9ef1f174fe34f435357b1891ee196d25ff63c0
EL5225 <luthfiramadhan155@gmail.com>
2023-11-06 20:28:48 +0700

    add sentry

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import swaggerUi from "swagger-ui-express";
 import docs from "./docs/swagger.json" assert { type: "json" };
 import cors from "cors";
 import "dotenv/config";
+import Sentry from "@sentry/node";
 import router from "./routes/index.js";
 import {
   errorHandler,
@@ -10,16 +11,44 @@ import {
   prismaErrorHandler,
   zodErrorHandler,
 } from "./middlewares/index.js";
-
+const { SENTRY_DNS } = process.env;
 const app = express();
-const swaggerDocument = docs;
+
+Sentry.init({
+  dsn: SENTRY_DNS,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+});
 
 app.use(cors());
 app.use(express.json());
+const swaggerDocument = docs;
 
-app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// All your controllers should live here
+app.get("/", (req, res) => {
+  res.json({
+    status: true,
+    message: "Welcome to API, open api docs at /api/docs",
+  });
+});
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1", router);
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
 app.use(zodErrorHandler);
 app.use(prismaErrorHandler);
 app.use(notFound);
